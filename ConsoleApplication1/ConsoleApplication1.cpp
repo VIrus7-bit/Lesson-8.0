02ea5b6c7fdbd62877a5016a61eb154ae828c2b4
VIrus7-bit <italyfotura7@gmail.com>
2024-12-20 06:54:31 -0800

    Lesson 8

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+
+template <typename T>
+void move_vectors(std::vector<T>& from, std::vector<T>& to) {
+
+    to.reserve(from.size()); 
+    for (auto& item : from) {
+        to.push_back(std::move(item)); 
+    }
+    from.clear(); 
+}
+
+int main() 
+{
+    using namespace std;
+
+    setlocale(LC_ALL, "Rus");
+
+    vector<std::string> one = { "test_string1", "test_string2" };
+    vector<std::string> two;
+
+    move_vectors(one, two);
+    
+    cout << "Vector one: ";
+    for (const auto& str : one) {
+        std::cout << str << " ";
+    }
+    cout << "\n";
+
+    cout << "Vector two: ";
+    for (const auto& str : two) {
+        std::cout << str << " ";
+    }
+    cout << "\n";
+
+    return EXIT_SUCCESS;
+}
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+class big_integer {
+private:
+    std::string number;
+
+public:
+    big_integer(const std::string& num) : number(num) {}
+
+    big_integer(big_integer&& other) noexcept : number(std::move(other.number)) {}
+
+    big_integer& operator=(big_integer&& other) noexcept {
+        if (this != &other) {
+            number = std::move(other.number);
+        }
+        return *this;
+    }
+
+    big_integer operator+(const big_integer& other) const {
+        std::string num1 = std::string(this->number.rbegin(), this->number.rend());
+        std::string num2 = std::string(other.number.rbegin(), other.number.rend());
+
+        std::string result;
+        int carry = 0;
+
+        size_t len1 = num1.length();
+        size_t len2 = num2.length();
+        size_t maxLength = std::max(len1, len2);
+
+        for (size_t i = 0; i < maxLength || carry; ++i) {
+            int digit1 = (i < len1) ? num1[i] - '0' : 0; 
+            int digit2 = (i < len2) ? num2[i] - '0' : 0;
+            int sum = digit1 + digit2 + carry;
+            result.push_back((sum % 10) + '0');
+            carry = sum / 10;
+        }
+
+        std::reverse(result.begin(), result.end());
+        return big_integer(result);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const big_integer& bi) {
+        os << bi.number;
+        return os;
+    }
+};
+
+int main() {
+
+    using namespace std;
+
+    setlocale(LC_ALL, "Rsu");
+
+    big_integer number1("114575");
+    big_integer number2("78524");
+    big_integer result = number1 + number2;
+
+    std::cout << result << std::endl;
+
+    return EXIT_SUCCESS;
+}
